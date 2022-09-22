21ccb8882d6a7a4914a13c4705af4d8e99484f97
Squiddleton <mdlegomaster@gmail.com>
2022-09-22 15:56:28 -0400

    Automatically listen for events, and reorganize

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,16 @@
-import { Client as BaseClient, ClientOptions as BaseClientOptions, Collection, Snowflake } from 'discord.js';
+import { Client as BaseClient, ClientEvents, ClientOptions as BaseClientOptions, Collection, Snowflake } from 'discord.js';
 import { ContextMenu, ContextMenuType, Scope, SlashCommand } from './commands';
+import { AnyClientEvent, ClientEvent } from './events';
 import { validateGuild } from './functions';
 
+export type AnyCommand = ContextMenu<ContextMenuType> | SlashCommand;
+
 export interface ClientOptions extends BaseClientOptions {
+	commands?: AnyCommand[];
+	/**
+	 * Events to automatically listen for when the Client is constructed
+	 */
+	events?: AnyClientEvent[];
 	/**
 	 * The id of a guild where all commands should be deployed
 	 */
@@ -13,37 +21,35 @@ export interface ClientOptions extends BaseClientOptions {
 	exclusiveGuildId?: Snowflake;
 }
 
-const readyError = (propertyName: string) => new Error(`The ${propertyName} property cannot be accessed until the Client is ready`);
-
 const localScopes: Scope[] = ['Dev', 'Exclusive'];
+const mapToColl = <T extends AnyCommand | AnyClientEvent>(k: T): [T['name'], T] => [k.name, k];
+const readyError = (propertyName: string) => new Error(`The ${propertyName} property cannot be accessed until the Client is ready`);
+const registerEvents = <T extends keyof ClientEvents>(client: BaseClient, event: ClientEvent<T>) => {
+	if (event.once) {
+		client.once(event.name, event.execute);
+	}
+	else {
+		client.on(event.name, event.execute);
+	}
+};
 
 /**
  * An extension of the discord.js Client with more constructor options, properties, and methods
  */
 export class Client<Ready extends boolean = boolean> extends BaseClient<Ready> {
-	commands: Collection<string, ContextMenu<ContextMenuType> | SlashCommand>;
+	commands: Collection<string, AnyCommand> = new Collection();
+	events: Collection<string, AnyClientEvent> = new Collection();
 	devGuildId: Snowflake | null;
 	exclusiveGuildId: Snowflake | null;
-	/**
-	 *
-	 */
-	get devGuild() {
-		if (!this.isReady()) throw readyError('devGuild');
-		const { devGuildId } = this;
-		if (devGuildId === null) return null;
-
-		return validateGuild(this, devGuildId);
-	}
-	get exclusiveGuild() {
-		if (!this.isReady()) throw readyError('exclusiveGuild');
-		const { exclusiveGuildId } = this;
-		if (exclusiveGuildId === null) return null;
-
-		return validateGuild(this, exclusiveGuildId);
-	}
 	constructor(options: ClientOptions) {
 		super(options);
-		this.commands = new Collection();
+		if (options.commands !== undefined) {
+			this.commands = new Collection(options.commands.map(mapToColl));
+		}
+		if (options.events !== undefined) {
+			this.events = new Collection(options.events.map(mapToColl));
+		}
+		this.events.forEach(event => registerEvents(this, event as ClientEvent<keyof ClientEvents>)); // TODO: Improve prevention of "too complex to represent" error
 		this.devGuildId = options.devGuildId ?? null;
 		this.exclusiveGuildId = options.exclusiveGuildId ?? null;
 	}
@@ -72,5 +78,22 @@ export class Client<Ready extends boolean = boolean> extends BaseClient<Ready> {
 				.map(c => c.toJSON())
 			);
 		}
+	}
+	/**
+	 *
+	 */
+	get devGuild() {
+		if (!this.isReady()) throw readyError('devGuild');
+		const { devGuildId } = this;
+		if (devGuildId === null) return null;
+
+		return validateGuild(this, devGuildId);
+	}
+	get exclusiveGuild() {
+		if (!this.isReady()) throw readyError('exclusiveGuild');
+		const { exclusiveGuildId } = this;
+		if (exclusiveGuildId === null) return null;
+
+		return validateGuild(this, exclusiveGuildId);
 	}
 }
--- src/events.ts
+++ src/events.ts
@@ -1,21 +1,26 @@
 import { Awaitable, ClientEvents } from 'discord.js';
 
-export interface ClientEventData<T extends keyof ClientEvents> {
+type ClientEventNames = keyof ClientEvents;
+type Execute<T extends ClientEventNames> = (...args: ClientEvents[T]) => Awaitable<void>;
+
+export interface ClientEventData<T extends ClientEventNames> {
 	/** The name of the ClientEvent */
 	name: T;
 	/** Whether to only listen for the event once */
 	once?: boolean;
 	/** A function with the event's parameters to call when the event emits */
-	execute: (...params: ClientEvents[T]) => Awaitable<void>;
+	execute: Execute<T>;
 }
 
-export class ClientEvent<T extends keyof ClientEvents> implements ClientEventData<T> {
+export class ClientEvent<T extends ClientEventNames> implements ClientEventData<T> {
 	name: T;
-	once = false;
-	execute: (...params: ClientEvents[T]) => Awaitable<void>;
+	once: boolean;
+	execute: Execute<T>;
 	constructor(data: ClientEventData<T>) {
 		this.name = data.name;
 		this.once = data.once ?? false;
 		this.execute = data.execute;
 	}
 }
+
+export type AnyClientEvent = { [T in ClientEventNames]: ClientEvent<T> }[ClientEventNames];
